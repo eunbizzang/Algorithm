a0d32808b869636ed12484523c5e7690bfb9b78d
eunbizzang <vysql123@naver.com>
2021-12-26 20:06:34 +0900

    1226 BinSearch

--- src/chap02/CardConvRev2.java
+++ src/chap02/CardConvRev2.java
@@ -2,7 +2,7 @@ package chap02;
 import java.util.Scanner;
 // 입력 받은 10진수를 2진수~36진수로 기수 변환하여 나타냄
 
-class CardConvRev {
+class CardConvRev2 {
 	// 정숫값 x를 r진수로 변환하여 배열 d에 아랫자리부터 넣어두고 자릿수를 반환합니다.
 	static int cardConvR(int x, int r, char[] d) {
 		int digits = 0;						// 변환 후의 자릿수
