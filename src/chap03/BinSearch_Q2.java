781a7d903f91b9c1714a00f7b6e5cb2453aace11
eunbizzang <vysql123@naver.com>
2021-12-29 09:42:41 +0900

    1229 BinarySearch, SeqSearch

--- src/chap03/BinSearch_Q2.java
+++ src/chap03/BinSearch_Q2.java
@@ -4,6 +4,32 @@ import java.util.Scanner;
 
 public class BinSearch_Q2 {
 
+	// book ver
+	static int seqSearchEx(int[] a, int n, int key) {
+		System.out.print("   |");
+		for (int k = 0; k < n; k++)
+			System.out.printf("%4d", k);
+		System.out.println();
+
+		System.out.print("---+");
+		for (int k = 0; k < 4 * n + 2; k++)
+			System.out.print("-");
+		System.out.println();
+
+		for (int i = 0; i < n; i++) {
+			System.out.print("   |");
+			System.out.printf(String.format("%%%ds*\n", (i * 4) + 3), "");
+			System.out.printf("%3d|", i);
+			for (int k = 0; k < n; k++)
+				System.out.printf("%4d", a[k]);
+			System.out.println("\n   |");
+			if (a[i] == key)
+				return i; // 검색성공
+		}
+		return -1; // 검색실패
+	}
+
+	// binary ver
 	static int binSearch(int[] x, int num, int key) {
 		int pl = 0;
 		int pr = num - 1;
@@ -17,25 +43,39 @@ public class BinSearch_Q2 {
 			} else {
 				pr = pc -1;
 			}
-		} while (pl > pr);
+		} while (pl <= pr);
 		return -1;
 	}
 	
 	// 각 행의 맨 왼쪽에 현재 검색하는 요소의 인덱스를 출력
 	// 현재 검색하고 있는 요소 위에 별표 기호를 출력
-	static void seqSearch(int[] x, int num, int key) {
+	static int seqSearch(int[] x, int num, int key) {
 		
-		System.out.println("  |");
+		System.out.print(" |");
 		for(int i=0; i<num; i++) {
 			System.out.printf("%2d", x[i]);
 		}
+		System.out.println();
+		System.out.println("-+-----------------");
 		
-		System.out.println("--------------------------");
-		
+		int check = -1;
 		for(int i=0; i<num; i++) {
-			System.out.println(i + "|");
 			
+			System.out.print(" |");
+			for(int j=0; j<i; j++) {
+				System.out.print("  ");
+			}
+			System.out.println(" *");
+			System.out.print(i + "|");
+			for(int d=0; d<num; d++) {
+				System.out.printf("%2d", x[d]);
+			}
+			System.out.println();
+			if(x[i] == key) {
+				check = i;
+			}
 		}
+		return check;
 	}
 	
 	public static void main(String[] args) {
@@ -56,20 +96,37 @@ public class BinSearch_Q2 {
 			} while (x[i-1] > x[i]);
 		}
 		
-		System.out.println("검색할 값 : ");
+		System.out.print("검색할 값 : ");
 		int key = sc.nextInt();
 		
 		System.out.println("선형검색 결과");
-		seqSearch(x, num, key);
+		int seq = seqSearch(x, num, key);
 		
-		System.out.println("바이너리 검색 결과.");
-		int check = binSearch(x, num, key);
-		
-		if(check == -1) {
+		if(seq == -1) {
 			System.out.println("검색한 값이 없습니다.");
 		}else {
-			System.out.println("검색한 값은 x[" + check + "]에 들어있음");
+			System.out.println("검색한 값은 x[" + seq + "]에 들어있음");
+		}
+		
+		System.out.println("바이너리 검색 결과.");
+		int bin = binSearch(x, num, key);
+		
+		if(bin == -1) {
+			System.out.println("검색한 값이 없습니다.");
+		}else {
+			System.out.println("검색한 값은 x[" + bin + "]에 들어있음");
+		}
+		
+		System.out.println("책 버전 검색 결과.");
+		int book = seqSearchEx(x, num, key);
+		
+		if(book == -1) {
+			System.out.println("검색한 값이 없습니다.");
+		}else {
+			System.out.println("검색한 값은 x[" + book + "]에 들어있음");
 		}
 	}
 
 }
+
+// book ver
